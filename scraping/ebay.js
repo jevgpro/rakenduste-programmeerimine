d75e771b33a4e149f0b2ae17552f35f12f790287
Jevgeni Prostsenko <43129604+jevgpro@users.noreply.github.com>
2019-09-19 23:51:13 +0300

    ulesanne 2 not working

---
+++ scraping/ebay.js
@@ -0,0 +1,45 @@
+// needs scope
+// <li class="s-item   "
+// <div class="s-item__image">
+// <h3 class="s-item__title s-item__title--has-tags">
+//<span class="s-item__price">$118.58</span>
+{
+    // EBAY
+    const itemContainerClass = "s-item   ";
+    const imageClass = "s-item__image";
+    const titleClass = "s-item__title s-item__title--has-tags";
+    const priceClass = "s-item__price";
+    // Amazon
+    // const itemContainerClass = "s-row";
+    // const imageClass = "s-image";
+    // const titleClass = "s-item__title s-item_title--has-tags";
+    // const priceClass = "s-item__price";
+    const items = document.getElementsByClassName(itemContainerClass);
+    const arr = [];
+
+    // HTMLDivElement[] to Array
+    Array.from(items).forEach(item => {
+        const imgs = item.getElementsByClassName(imageClass);
+        if(imgs.length === 0 ) return; //some have 0, skip them
+        const img = imgs[0];
+
+        const src = img.dataset.src;
+
+        if(!src) return; //skip these
+         
+        const title = item.getElementsByClassName(titleClass)[0].textContent;
+        const price = item.getElementsByClassName(priceClass)[0].textContent;
+
+   console.log("src", src);
+   console.log("title", title);
+   console.log("price", price);
+        arr.push({
+            imgSrc: src,
+            title,
+            price,
+            category: document.title.split("|")[0].trim()
+        })
+
+    });
+   console.log(JSON.stringify(arr));
+}
