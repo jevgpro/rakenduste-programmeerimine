2e8bbd6f6d7060e9e2a880e29a0ea9f6d9f45bdc
jevgpro <prostsenko2@gmail.com>
2019-09-20 11:37:25 +0300

    ulesanne 3 tehtud

--- scraping/ebay.js
+++ scraping/ebay.js
@@ -6,8 +6,8 @@
 {
     // EBAY
     const itemContainerClass = "s-item   ";
-    const imageClass = "s-item__image";
-    const titleClass = "s-item__title s-item__title--has-tags";
+    const imageClass = "s-item__image-img";
+    const titleClass = "s-item__title";
     const priceClass = "s-item__price";
     // Amazon
     // const itemContainerClass = "s-row";
@@ -16,30 +16,33 @@
     // const priceClass = "s-item__price";
     const items = document.getElementsByClassName(itemContainerClass);
     const arr = [];
-
     // HTMLDivElement[] to Array
     Array.from(items).forEach(item => {
         const imgs = item.getElementsByClassName(imageClass);
         if(imgs.length === 0 ) return; //some have 0, skip them
         const img = imgs[0];
 
-        const src = img.dataset.src;
+        const src = img.src;
 
         if(!src) return; //skip these
+        // if(!title) return; //skip these
          
         const title = item.getElementsByClassName(titleClass)[0].textContent;
         const price = item.getElementsByClassName(priceClass)[0].textContent;
-
-   console.log("src", src);
-   console.log("title", title);
-   console.log("price", price);
         arr.push({
             imgSrc: src,
             title,
             price,
             category: document.title.split("|")[0].trim()
         })
+        // // console.log("src", src);
+        // console.log("title", title);
+        // console.log("price", price);
+        // console.log("src", src);
+
+        // // console.log("price", price);
+        // console.log("category", category);
 
     });
-   console.log(JSON.stringify(arr));
+    console.log(JSON.stringify(arr));
 }
