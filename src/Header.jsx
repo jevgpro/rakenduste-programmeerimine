acc61390c218cea69ad5ad1ea532b438efd98194
Jevgeni Prostsenko <43129604+jevgpro@users.noreply.github.com>
2019-10-01 22:49:41 +0300

    Praktikum Task 7 pooleni(doesn't work)

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+const Header = () => {
+    return(
+        <div className="header">
+        <img class="header_logo" src="./images/logo.jpg" />
+        <div class="header_buttons">
+            <button>Login/Signup</button>
+            <button>Cart</button>
+        </div>
+    </div>
+    )
+};
+
+export default Header;
