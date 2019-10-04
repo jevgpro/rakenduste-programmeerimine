460d7a2e7391d139161873ebfe6ce951c5723937
jevgpro <prostsenko2@gmail.com>
2019-10-04 11:38:59 +0300

    splited jsx files

---
+++ src/ItemPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header.jsx";
+
+class ItemPage extends React.PureComponent{
+    render(){
+        return (
+            <>
+            <Header/>
+            Item
+            </>
+        )
+    }
+}
+
+export default ItemPage;
