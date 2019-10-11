d9d2b4028ec52cdef7861488998c2ee9a78628f6
Jevgeni Prostsenko <43129604+jevgpro@users.noreply.github.com>
2019-10-11 10:21:18 +0300

    Praktikum 4, error need to fix

--- src/ItemPage.jsx
+++ src/ItemPage.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import Header from "./Header.jsx";
 import {phones} from "./mydatabase.js";
+import PropTypes from "prop-types";
 
 class ItemPage extends React.PureComponent{
+    componentDidMount(){
+        fetch("/api/items/${this.props.match.params.itemId}")
+        .then( res =>{
+            return res.json();
+        })
+        .then ( item=>{
+            console.log("item", item);
+        })
+        .catch(err =>{
+            console.log("item page", err);
+        });
+    }
+
     render(){
+        console.log("this.props", this.props);
+        console.log("itemID", this.props.match.params.itemId);
         const item = phones[0];
         return (
             <>
             <Header/>
             <div className={"itemContainer"}>
                 <img src={item.imgSrc} />
-                <div className={'item__title'}>{item.title}</div>
-                <div className={'item__price'}>{item.price}</div>
+                <div className={"item__title"}>{item.title}</div>
+                <div className={"item__price"}>{item.price}</div>
             </div>
             </>
-        )
+        );
     }
 }
+
+ItemPage.propTypes = {
+    match: PropTypes.object.isRequired,
+};
 
 export default ItemPage;
--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./HomePage.jsx";
 import ItemPage from "./ItemPage.jsx";
-import { BrowserRouter, Route, Link} from "react-router-dom";
+import { BrowserRouter, Route} from "react-router-dom";
 
 const root = document.getElementById("app");
+
+// console.log("item id", itemId);
 
 ReactDOM.render(
     <BrowserRouter>
     <Route path="/" exact component={HomePage} />
-    <Route path="/item" exact component={ItemPage} />
+    <Route path="/items/:itemId" exact component={ItemPage} />
     </BrowserRouter>,
     root
 );
